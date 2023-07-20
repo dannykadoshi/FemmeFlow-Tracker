4624a2ccf43b7cb1ac1722d3bbcd57a05f71c0ae
dannykadoshi <>
2023-07-20 16:21:17 +0000

    Prompt user with option to open google form

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import datetime
+import webbrowser
+import pyfiglet
 import gspread
 from google.oauth2.service_account import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
@@ -16,6 +18,26 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('FemmeFlow Tracker (Responses)')
 
 responses = SHEET.worksheet('responses')
+
+
+# Open google form in the browser
+def open_google_form():
+    form_url = "https://forms.gle/ja7VxdgBAutRLz348"
+    webbrowser.open(form_url)
+
+
+# Prompt the user with an option to open the Google Form
+print("Would you like to enter your information in the Google Form? (yes/no)")
+response = input().strip().lower()
+
+# If the user enters 'yes', open the Google Form in the web browser
+if response == 'yes':
+    open_google_form()
+    print("Google Form opened in your web browser. Please enter your information there.")
+    print("After entering the information, you can come back to this terminal to see the calculated next period date.")
+else:
+    print("You chose not to enter your information in the Google Form.")
+
 
 # Get the last row of data in the Google Sheets
 data = responses.get_all_values()
@@ -36,15 +58,9 @@ last_period = datetime.datetime.strptime(last_period_str, "%d/%m/%Y").date()
 cycle_length = int(cycle_length_str)
 period_duration = int(period_duration_str)
 
-# Print the fetched data
-print("Last Form Submission Data:")
-print(f"Timestamp: {timestamp}")
-print(f"Last Period Date: {last_period}")
-print(f"Cycle Length: {cycle_length} days")
-print(f"Period Duration: {period_duration} days")
-print(f"Cycle Type: {cycle_type}")
-print(f"Cycle Lengths (if irregular): {cycle_lengths}")
-print(f"Symptoms/Additional Information: {symptoms}")
-print(f"Email: {email}")
-print(f"Name: {name}")
-print(f"Age: {age}")
+
+# Calculate the next period date
+next_period = last_period + datetime.timedelta(days=cycle_length)
+
+# Print the calculated next period date
+print(f"Next Period Date: {next_period.strftime('%d/%m/%Y')}")
